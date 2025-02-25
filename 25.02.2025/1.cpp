0b4d46f904be9c4566df7d1c2d1cb888bf567fe2
Ekaterina Gileva <125045167+Ekaterina132479@users.noreply.github.com>
2025-02-25 13:05:38 +0300

    Add files via upload

---
+++ 25.02.2025/1.cpp
@@ -0,0 +1,34 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+    // Загружаем изображение
+    Mat image = imread("C:/Users/gilyo/duck_and_beer.jfif");
+    if (image.empty()) {
+        cout << "Ошибка загрузки изображения!" << endl;
+        return -1;
+    }
+
+    // Преобразование в различные цветовые пространства
+    Mat image_hsv, image_lab, image_yuv, image_xyz, image_gray;
+    cvtColor(image, image_hsv, COLOR_BGR2HSV);
+    cvtColor(image, image_lab, COLOR_BGR2Lab);
+    cvtColor(image, image_yuv, COLOR_BGR2YUV);
+    cvtColor(image, image_xyz, COLOR_BGR2XYZ);
+    cvtColor(image, image_gray, COLOR_BGR2GRAY);
+
+    // Отображение изображений
+    imshow("Original (BGR)", image);
+    imshow("HSV", image_hsv);
+    imshow("Lab", image_lab);
+    imshow("YUV", image_yuv);
+    imshow("XYZ", image_xyz);
+    imshow("Grayscale", image_gray);
+
+    // Ожидание нажатия клавиши перед выходом
+    waitKey(0);
+    return 0;
+}
